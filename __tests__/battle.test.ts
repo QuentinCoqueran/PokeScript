3fecba65ee1a715616f3d36c895df1338c8b2402
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2022-04-27 09:14:35 +0200

    test describe

---
+++ __tests__/battle.test.ts
@@ -0,0 +1,25 @@
+describe("Battle", () => {
+    describe("first to attack", () => {
+        describe("when Pikachu and Salamèche fights ", () => {
+            it.todo("should return Pikachu as first to attack");
+        });
+        describe("when Salamèche and Bulbizarre fights ", () => {
+            it.todo("should return Salamèche as first to attack");
+        });
+        describe("when Pikachu and Bulbizarre fights ", () => {
+            it.todo("should return Pikachu as first to attack");
+        });
+    });
+    describe("Winners", () => {
+        describe("when Pikachu and Salamèche fights ", () => {
+            it.todo("should return Pikachu as winner");
+        });
+        describe("when Salamèche and Bulbizarre fights ", () => {
+            it.todo("should return Salamèche as winner");
+        });
+        describe("when Pikachu and Bulbizarre fights ", () => {
+            it.todo("should return Pikachu as winner");
+        });
+    });
+});
+
