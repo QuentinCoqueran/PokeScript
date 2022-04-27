47c02b2d13cfae26d6fd0438d14e9f385c654d33
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2022-04-27 12:04:22 +0200

    gitignore

--- index.ts
+++ index.ts
@@ -1,47 +1,10 @@
 import {Pokemon} from "./pokemon";
+import {Battle} from "./battle";
 
-export function Battle(pokemon1 : Pokemon, pokemon2 : Pokemon): string {
-    let countRound = 0;
-    let p1AsAttack = false;
-    while(pokemon1.currentHp <= 0 || pokemon2.currentHp <=0){
-        if(countRound === 0){
-            if(FirstToAttack(pokemon1,pokemon2) === pokemon1){
-                pokemon1.attackTo(pokemon2);
-            }
-            else{
-                pokemon2.attackTo(pokemon1);
-            }
-            countRound ++;
-        }
-        if(p1AsAttack){
-            pokemon2.attackTo(pokemon1);
-            p1AsAttack = false;
-        }
-        else{
-            pokemon1.attackTo(pokemon2);
-            p1AsAttack = true;
-        }
-    }
-    if(pokemon1.currentHp <= 0){
-        return `${pokemon2.name} is the winner !`
-    }
-    else{
-        return `${pokemon1.name} is the winner !`
-    }
-}
 
-export function FirstToAttack (pokemon1 : Pokemon, pokemon2 : Pokemon): Pokemon {
-    if(pokemon1.speed > pokemon2.speed){
-        return pokemon1;
-    }
-    else{
-        return pokemon2;
-    }
-}
-
+const battle : Battle = new Battle();
 const pikachu : Pokemon = new Pokemon('Pikachu', 25,5,10);
 const bulbizarre : Pokemon = new Pokemon('bulbizarre', 25,5,5);
 const salamèche : Pokemon = new Pokemon('salamèche', 25,6,8);
-
 
 
