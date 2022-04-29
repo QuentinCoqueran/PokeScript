45b0292d7fe1411fd9e890ec294d3ce02b107f13
Marwan <marwan.boubchir@outlook.fr>
2022-04-29 09:51:53 +0200

    feat(test): Ajout Asynchrone

--- src/battle.ts
+++ src/battle.ts
@@ -2,51 +2,49 @@ import {isPokemonFire, isPokemonIce, Pokemon} from "./pokemon";
 
 export class Battle {
 
-    battleRounds(pokemon1 : Pokemon, pokemon2 : Pokemon): string {
+    async battleRounds(pokemon1: Pokemon, pokemon2: Pokemon): Promise<string> {
         let countRound = 0;
         let p1AsAttack = false;
-        while(pokemon1.currentHp <= 0 || pokemon2.currentHp <=0){
-            if(countRound === 0){
-                if(this.FirstToAttack(pokemon1,pokemon2) === pokemon1){
-                    this.attack(pokemon1, pokemon2)
+        while (pokemon1.currentHp <= 0 || pokemon2.currentHp <= 0) {
+            if (countRound === 0) {
+                if (this.FirstToAttack(pokemon1, pokemon2) === pokemon1) {
+                    await this.attack(pokemon1, pokemon2)
+                } else {
+                    await this.attack(pokemon2, pokemon1)
                 }
-                else{
-                    this.attack(pokemon2, pokemon1)
-
-                }
-                countRound ++;
+                countRound++;
             }
-            if(p1AsAttack){
-                this.attack(pokemon2, pokemon1)
+            if (p1AsAttack) {
+                await this.attack(pokemon2, pokemon1)
 
                 p1AsAttack = false;
-            }
-            else{
-                this.attack(pokemon1, pokemon2)
-
+            } else {
+                await this.attack(pokemon1, pokemon2);
                 p1AsAttack = true;
             }
         }
-        if(pokemon1.currentHp <= 0){
+        if (pokemon1.currentHp <= 0) {
             return `${pokemon2.name} is the winner !`
-        }
-        else{
+        } else {
             return `${pokemon1.name} is the winner !`
         }
     }
 
-    attack(from: Pokemon, to: Pokemon){
-        let coef : number = 1;
-        if(isPokemonFire(from)&& isPokemonIce(to)){
-            coef = 1.5;
-        }
-        if(to.currentHp > from.attack * coef) {
-            to.currentHp -= from.attack * coef;
-        }
-        else {
-            to.currentHp = 0;
-        }
-
+    async attack (from: Pokemon, to: Pokemon){
+        return new Promise(function (resolve, reject){
+            setTimeout(()=>{
+                let coef : number = 1;
+                if(isPokemonFire(from)&& isPokemonIce(to)){
+                    coef = 1.5;
+                }
+                if(to.currentHp > from.attack * coef) {
+                    to.currentHp -= from.attack * coef;
+                }
+                else {
+                    to.currentHp = 0;
+                }
+            },5);
+        });
     }
 
     FirstToAttack (pokemon1 : Pokemon, pokemon2 : Pokemon): Pokemon {
